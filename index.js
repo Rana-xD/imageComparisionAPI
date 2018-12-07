eec528fa58677edd2999bc818ff53fd8e8b43302
Ubuntu <ubuntu@ip-172-31-4-8.ap-southeast-1.compute.internal>
2018-12-07 02:29:21 +0000

    alter index.js code

--- index.js
+++ index.js
@@ -7,18 +7,17 @@ var fs = require('fs');
 
 
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-
 /**bodyParser.json(options)
  * Parses the text as JSON and exposes the resulting object on req.body.
  */
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.get('/', (req, res, next) => {
     res.status(200);
-    res.send({status:'OK'});
+    res.send({status:'OK', message:'Ready to rock.'});
 });
 
 app.post('/', (req, res) => {
@@ -26,7 +25,7 @@ app.post('/', (req, res) => {
     var markupImage = req.body.markup;
     var pathDesignImage, pathMarkupImage;
     var width = req.body.width;
-
+    width = parseInt(width);
 
     var pathDesignImagePromise = aws.downloadImage(designImage,width);
     pathDesignImagePromise.then((path) => {
@@ -39,8 +38,10 @@ app.post('/', (req, res) => {
             {
                 compareImage.deleteFile(pathDesignImage);
                 compareImage.deleteFile(pathMarkupImage);
+		res.status(200);
                 res.send({
                     code: "success",
+		    statusCode: 200,
                     meesage:"same"
                 })
             }
@@ -49,75 +50,97 @@ app.post('/', (req, res) => {
                     compareImage.deleteFile(pathDesignImage);
                     compareImage.deleteFile(pathMarkupImage);
                     compareImage.getAveragePixelsCoords(coordinate).then((average)=>{
+			res.status(200);
                         res.send({
                             code: "success",
+			    statusCode: 200,
                             message: "different",
                             coordinate: average
                         })
                     },(error)=>{
                         console.log("ERROR WHEN CREAT AVERAGE"+error);
+			res.status(500);
                         res.send({
                             code: "fail",
+			    statusCode: 500,
                             message: error
                         });
                     }).catch((error)=>{
                         console.log("ERROR WHEN CREAT AVERAGE"+error);
+			res.status(500);
                         res.send({
                             code: "fail",
+			    statusCode: 500,
                             message: error
                         });
                     })
                     
                 },(error)=>{
                     console.log("ERROR WHEN CREAT COORDIDATE"+error);
+		    res.status(500);
                     res.send({
                         code: "fail",
+			statusCode: 500,
                         message: error
                     });
                 }).catch((error)=>{
                     console.log("ERROR WHEN CREAT COORDIDATE"+error);
+		    res.status(500);
                     res.send({
                         code: "fail",
+			statusCode: 500,
                         message: error
                     });
                 });
             }
             },(error)=>{
                 console.log("ERROR WHILE COMPARE: "+error);
+		res.status(500);
                 res.send({
                     code: "fail",
+		    statusCode: 500,
                     message: error
                 });
             }).catch((error)=>{
                 console.log("ERROR WHILE COMPARE: "+error);
+		res.status(500);
                 res.send({
                     code: "fail",
+		    statusCode: 500,
                     message: error
                 });
             });
            },(error)=>{
             console.log("ERROR WHEN RESIZE MARKUP IMAGE: "+err);
+	    res.status(500);
             res.send({
                 code: "fail",
+		statusCode: 500,
                 message: error
             }); 
            }).catch((err)=>{
             console.log("ERROR WHEN RESIZE MARKUP IMAGE: "+err);
+	    res.status(500);
             res.send({
                 code: "fail",
+		statusCode: 500,
                 message: error
             });
            });
     }, (error) => {
         console.log("ERROR GET PATH DESIGN IMAGE: "+error);
+	res.status(500);
         res.send({
             code: "fail",
+	    statusCode: 500,
             message: error
         });
     }).catch((error) => {
         console.log("ERROR GET PATH DESIGN IMAGE: "+error);
+	res.status(500);
         res.send({
             code: "fail",
+	    statusCode: 500,
             message: error
         });
     });
